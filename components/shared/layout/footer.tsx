e44d2627249953ed47575b58c0400b68ceb53ff1
IjlalWindhi <ijlalwindhi15@gmail.com>
2024-06-21 17:12:56 +0700

    UI(footer): text dark mode

--- components/shared/layout/footer.tsx
+++ components/shared/layout/footer.tsx
@@ -5,7 +5,7 @@ function footer() {
   const year = () => new Date().getFullYear();
 
   return (
-    <footer className="px-6 py-4 inline-flex items-center justify-center bg-secondary text-center font-sans text-xs text-slate-300 dark:bg-primary-foreground">
+    <footer className="px-6 py-4 inline-flex items-center justify-center bg-secondary text-center font-sans text-xs dark:text-slate-300 dark:bg-primary-foreground">
       &copy;{year()}. Dhisa Production. All Righs Reserved.
     </footer>
   );
