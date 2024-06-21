c75df97d5f9778f164214ef04f3ed50e81990846
IjlalWindhi <ijlalwindhi15@gmail.com>
2024-06-21 16:46:34 +0700

    change(constants): sidebar menu and profile menu

--- constants/index.ts
+++ constants/index.ts
@@ -1,24 +1,63 @@
-import { FiHome } from "react-icons/fi";
-import { IoDocumentTextOutline } from "react-icons/io5";
+import { LayoutDashboard, SquareCheck } from "lucide-react";
 
 export const navLinks: INavLink[] = [
   {
-    label: "Home",
+    label: "Dashboard",
     route: "/",
-    icon: FiHome,
+    icon: LayoutDashboard,
     sub: [],
     isOpen: false,
   },
   {
-    label: "Project",
-    route: "/project",
-    icon: IoDocumentTextOutline,
-    sub: [
-      {
-        label: "Internal",
-        route: "/project",
-      },
-    ],
+    label: "Task",
+    route: "/task",
+    icon: SquareCheck,
+    sub: [],
     isOpen: false,
+  },
+  // {
+  //   label: "Project",
+  //   route: "/project",
+  //   icon: File,
+  //   sub: [
+  //     {
+  //       label: "Internal",
+  //       route: "/project",
+  //     },
+  //   ],
+  //   isOpen: false,
+  // },
+];
+
+export const profileMenu: IProfileMenu[] = [
+  {
+    label: "My Account",
+    route: null,
+    type: "label",
+  },
+  {
+    label: null,
+    route: null,
+    type: "separator",
+  },
+  {
+    label: "Settings",
+    route: "/settings",
+    type: "item",
+  },
+  {
+    label: "Support",
+    route: "/support",
+    type: "item",
+  },
+  {
+    label: null,
+    route: null,
+    type: "separator",
+  },
+  {
+    label: "Logout",
+    route: "/logout",
+    type: "item",
   },
 ];
