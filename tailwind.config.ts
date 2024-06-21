9aebf9829a5334e17bb62bc2fc084512374e0770
IjlalWindhi <ijlalwindhi15@gmail.com>
2024-06-21 16:37:15 +0700

    feat(tailwind config): screens xs for min-width 375px

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,7 @@ const config = {
   ],
   prefix: "",
   theme: {
+    fontFamily: {},
     container: {
       center: true,
       padding: "2rem",
@@ -72,6 +73,25 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+    },
+    screens: {
+      xs: "375px",
+      // => @media (min-width: 375px) { ... }
+
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
     },
   },
   plugins: [require("tailwindcss-animate")],
